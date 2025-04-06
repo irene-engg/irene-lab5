528b74b4b1726603171f0b470832f4ad3062eba8
irene-engg <irene.engg2011@gmail.com>
2025-04-06 01:34:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello from Joseph ECS Cont!'
+  return 'Hello from Joseph ECS Container and to see if revision works!'
 
 app.run(host='0.0.0.0')
